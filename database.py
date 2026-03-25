980c3f62e3269586bf9bc5b14d96fc54dbb26a08
lohitha <lohitha0704@gmail.com>
2026-03-25 16:55:13 -0500

    feat: add GitHub Actions CI pipeline with pytest

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres123@localhost/opportunity_db"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres123@localhost/opportunity_db")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
